8a3be35cdfe736b44c6f59e1eb1cb951f80768a3
enkyn <me@enkyn.net>
2023-12-02 20:12:54 -0700

    Day 03 - Rust stub.

---
+++ 2023/rust/day03/src/lib.rs
@@ -0,0 +1,9 @@
+include!("../../wasm-glue.rs");
+
+fn part_one(input_buf: &[u8]) -> Vec<u8> {
+    unimplemented!("part_one")
+}
+
+fn part_two(input_buf: &[u8]) -> Vec<u8> {
+    unimplemented!("part_two")
+}

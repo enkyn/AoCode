5e884d928b93c6eb91e439c487289eb50d8f6337
enkyn <me@enkyn.net>
2023-12-07 18:33:17 -0700

    Day 6 - Rust - Stub

---
+++ 2023/rust/day06/src/lib.rs
@@ -0,0 +1,19 @@
+include!("../../wasm-glue.rs");
+
+fn part_one(input_buf: &[u8]) -> Vec<u8> {
+    todo!()
+}
+
+fn part_two(input_buf: &[u8]) -> Vec<u8> {
+    todo!()
+}
+
+#[test]
+fn part_one_example() {
+    todo!()
+}
+
+#[test]
+fn part_two_example() {
+    todo!()
+}
